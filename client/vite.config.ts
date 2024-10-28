4280411aacfccd8258c120327030e904ec252eff
prathamdas <prathamdas003@gmail.com>
2024-10-29 03:44:34 +0530

    build(app): cors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,12 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://clerk.calender-view.vercel.dev',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
   }
 })
